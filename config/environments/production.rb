c6374452f7d3d81ed70f6e3f2e3cbed0afcb1906
Jim <jim1318@gmail.com>
2017-05-04 09:34:55 -0500

    updating email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,11 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "bcasual.com",
+    domain: "infinite-tundra-47271herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
+
 end
